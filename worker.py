c1071aefbbcab2174ca1489966b11ac018981d42
Elahe <1997.esm@gmail.com>
2022-03-13 13:12:02 +0330

    :art: BLACK

--- worker.py
+++ worker.py
@@ -1,20 +1,21 @@
 import sys
 
 import redis
-from rq import Worker, Queue, Connection
+from rq import Connection, Queue, Worker
 
 from note_cast.core.settings import settings
+
 redis_conn = redis.from_url(settings.REDIS_URL)
 listen = settings.REDIS_LISTEN
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     queue_name = sys.argv[1]
-   
+
     if queue_name in listen:
-        with Connection(redis_conn):  
+        with Connection(redis_conn):
             worker = Worker(map(Queue, [queue_name]))
             worker.work(with_scheduler=True)
     else:
-        print('invalid queue name passed')
+        print("invalid queue name passed")
