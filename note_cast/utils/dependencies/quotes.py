01db9ca9f744d8ed62726ba3ab26674f9272de90
Elahe <1997.esm@gmail.com>
2022-06-11 20:04:59 +0430

    :fire: remove old submit_quote dependency function

--- note_cast/utils/dependencies/quotes.py
+++ note_cast/utils/dependencies/quotes.py
@@ -53,46 +53,6 @@ def submit_quote(quote_data: NewQuoteMetadata, quote_upload_response : dict = De
         return new_quote, parent_episode, parent_podcast
 
 
-def _submit_quote(quote_data: NewQuoteMetadata):
-    from note_cast.utils import DataBaseUtils
-    from note_cast.api.errors.cloudinary_exception import CloudinaryUploadException
-    from note_cast.services.cloudinary_api.uploader import CloudinaryUpload
-
-    try:
-
-        upload_response = CloudinaryUpload.upload_quote(data=quote_data)
-
-        if upload_response.status:
-
-            (
-                new_quote,
-                parent_episode,
-                parent_podcast,
-            ) = DataBaseUtils.get_or_create_submitting_quote_parents(
-                asset_public_id=upload_response.asset_public_id,
-                data=quote_data
-            )
-
-            if upload_response.data["status"] != "pending":
-
-                CloudinaryUpload.prep_transcription_job_on_completed_upload(
-                    q_id=upload_response.asset_public_id,
-                    p_id=quote_data.p_id,
-                    e_id=quote_data.e_id,
-                    audio_segment_url=upload_response.data["url"],
-                )
-
-            return new_quote, parent_episode, parent_podcast
-
-    except CloudinaryUploadException as exc:
-        loguru_app_logger.exception(exc.message)
-        raise CustomHTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    except Exception as exc:
-        loguru_app_logger.exception(exc)
-        raise CustomHTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
 def quote_query_params(
     #TODO
     # timestamp: MentionRel = Query(None), 
